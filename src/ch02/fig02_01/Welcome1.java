95dbd76fef38bac1dc5ee1a8411db86e21946e16
Andre Carvalho Filho <11andrecarvalho@gmail.com>
2024-02-13 22:51:03 -0300

    idk

---
+++ src/ch02/fig02_01/Welcome1.java
@@ -0,0 +1,17 @@
+// Text-printing program.
+// coxadefrango
+// Last modified 13/02/2024 10:05.
+/* This is a test comment
+split over multiple lines */
+
+public class Welcome1 
+{
+	// main method begins execution of Java application
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Java Programming!");
+	} //end method main
+} // end class Welcome1
+
+
+//TODO learn about javadoc.
